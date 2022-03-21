451d4ee007f3853c5e16ec49da3e7e72991c5c61
Firebreak-2 <69664260+Firebreak-2@users.noreply.github.com>
2022-03-21 09:25:18 +0400

    Fixed issue where DoodleOnlyListener.cs would not be pushed to GitHub

---
+++ TLCBot2/Listeners/DoodleOnlyListener.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using Discord.WebSocket;
+using TLCBot2.Core;
+
+namespace TLCBot2.Listeners;
+
+public static class DoodleOnlyListener
+{
+    public static Task OnMessageRecieved(SocketMessage message)
+    {
+        if (message.Channel.Id == RuntimeConfig.DoodleOnlyChannel.Id && message.Attachments.Count == 0)
+        {
+            message.Author.SendMessageAsync(
+                $"Please do not speak in <#{RuntimeConfig.DoodleOnlyChannel.Id}>." +
+                "The channel is meant for people to speak only through drawings and doodles, not text.");
+            message.DeleteAsync();
+        }
+        return Task.CompletedTask;
+    }
+}
